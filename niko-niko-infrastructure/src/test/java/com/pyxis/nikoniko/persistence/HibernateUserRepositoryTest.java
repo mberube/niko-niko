eade89363def7868adc0cfd37ce826662c493e58
Mathieu Berube <mberube@pyxis-tech.com>
2011-01-06 17:07:50 -0500

    remove warnings

--- niko-niko-infrastructure/src/test/java/com/pyxis/nikoniko/persistence/HibernateUserRepositoryTest.java
+++ niko-niko-infrastructure/src/test/java/com/pyxis/nikoniko/persistence/HibernateUserRepositoryTest.java
@@ -1,9 +1,8 @@
 package com.pyxis.nikoniko.persistence;
 
 import static com.pyxis.nikoniko.persistence.testutils.PersistenceContext.get;
-import static junit.framework.Assert.fail;
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import javax.validation.ConstraintViolationException;
 
