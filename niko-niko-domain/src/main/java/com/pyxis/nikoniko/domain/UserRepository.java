3b86f6131b80f92d0c8f58e5130614df12e7ba19
Mathieu Berube <mberube@pyxis-tech.com>
2011-01-11 16:08:14 -0500

    format code

--- niko-niko-domain/src/main/java/com/pyxis/nikoniko/domain/UserRepository.java
+++ niko-niko-domain/src/main/java/com/pyxis/nikoniko/domain/UserRepository.java
@@ -3,7 +3,7 @@ package com.pyxis.nikoniko.domain;
 import java.util.List;
 
 public interface UserRepository {
-	void add(User user);
-	List<User> list();
-	Maybe<User> findByName(String username);
+    void add(User user);
+    List<User> list();
+    Maybe<User> findByName(String username);
 }
