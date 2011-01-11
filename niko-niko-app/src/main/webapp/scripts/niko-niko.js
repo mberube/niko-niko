c9f0d47a38f264f332cb5eb7b31f7823f913b6de
Mathieu Berube <mberube@pyxis-tech.com>
2011-01-11 17:23:09 -0500

    load users from db

--- niko-niko-app/src/main/webapp/scripts/niko-niko.js
+++ niko-niko-app/src/main/webapp/scripts/niko-niko.js
@@ -5,85 +5,96 @@ var backend;
 var calendar;
 var addUserBox;
 
-
 $(document).ready(function() {
 
-    backend = new Backend();
-    calendar = new Calendar();
-    calendar.initialize();
+	backend = new Backend();
+	calendar = new Calendar();
+	calendar.initialize();
 
-    addUserBox = new AddUserBox();
-    addUserBox.initialize();
+	addUserBox = new AddUserBox();
+	addUserBox.initialize();
 
 });
 
-function Calendar(){
+function Calendar() {
 
+	this.initialize = function() {
+		this.fill();
+	}
 
-    this.initialize =  function(){
-          this.fill();
-    }
+	this.fill = function() {
+		backend.users()
+		
+//		$(calendarSelector).table({
+//			replace : false,
+//			//data : backend.users()
+//			data : [["bob"], ["bob3"]]
+//		});
 
-    this.fill = function(){
-         $(calendarSelector).table({
-            replace : false,
-            data : backend.users()
-            });
-
-    }
-
-}
-
-
-function AddUserBox(){
-
-
-    this.initialize =  function(){
-
-        $(addUserBoxSelector).addClass("unfocusAddUserBox");
-
-        $(addUserBoxSelector).focus(function() {
-             $(this).removeClass("unfocusAddUserBox").addClass("focusAddUserBox");
-             this.value = '';
-
-        });
-
-        $(addUserBoxSelector).blur(function() {
-            backend.saveUser(this.value);
-            $(this).removeClass("focusAddUserBox").addClass("unfocusAddUserBox");
-	        this.value =  this.defaultValue;
-        });
-
-        $(addUserBoxSelector).keypress(function (keyPressed) {
-        	
-            var isEnterKeyPressed =  (keyPressed.which && keyPressed.which == 13) || (keyPressed.keyCode && keyPressed.keyCode == 13);
-            if (isEnterKeyPressed ) {
-            
-            	if (navigator.appName == 'Microsoft Internet Explorer')$(calendarSelector).focus();
-            	else $(this).blur();
-            }
-        });
-
-    }
-
+	}
 
 }
 
+function AddUserBox() {
 
+	this.initialize = function() {
 
-function Backend (){
+		$(addUserBoxSelector).addClass("unfocusAddUserBox");
 
-    this.innerList = new Array();
+		$(addUserBoxSelector).focus(
+				function() {
+					$(this).removeClass("unfocusAddUserBox").addClass(
+							"focusAddUserBox");
+					this.value = '';
 
-    this.saveUser = function (newUsername){
-        var username = $.trim(newUsername);
-        this.innerList[this.innerList.length] = new Array(username);
-        calendar.fill();
-    }
+				});
 
-    this.users = function(){
-          return this.innerList;
-    }
+		$(addUserBoxSelector).blur(
+				function() {
+					backend.saveUser(this.value);
+					$(this).removeClass("focusAddUserBox").addClass(
+							"unfocusAddUserBox");
+					this.value = this.defaultValue;
+				});
+
+		$(addUserBoxSelector)
+				.keypress(
+						function(keyPressed) {
+
+							var isEnterKeyPressed = (keyPressed.which && keyPressed.which == 13)
+									|| (keyPressed.keyCode && keyPressed.keyCode == 13);
+							if (isEnterKeyPressed) {
+
+								if (navigator.appName == 'Microsoft Internet Explorer')
+									$(calendarSelector).focus();
+								else
+									$(this).blur();
+							}
+						});
+
+	}
+
 }
 
+function Backend() {
 
+	this.innerList = new Array();
+
+	this.saveUser = function(newUsername) {
+		var username = $.trim(newUsername);
+		this.innerList[this.innerList.length] = new Array(username);
+		calendar.fill();
+	}
+	this.users = function() {
+		$.getJSON('calendar/users', "", function(users) {
+			$(calendarSelector).table({
+				replace : false,
+
+				// transform array into array of arrays
+				data : users.map(function(user) {
+					return [user];
+				})
+			});
+		});
+	}
+}
